5e345408f2d81407495ed1ce38afd2fd3e326c8e
emejulucodes <emmanueljunior433@gmail.com>
2023-12-16 04:04:44 +0100

    Update Firebase configuration

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,12 +1,75 @@
+const _0x5e7369 = _0x2a8c;
+function _0xbbe3() {
+  const _0x33a7fe = [
+    "AIzaSyAg_duJHI5WZxInEmIfr3DqKsTTw5eJYvk",
+    "1632450eIEVVd",
+    "40XcmFAi",
+    "546899xFiuVD",
+    "2YyCELK",
+    "300bGlrjE",
+    "38646gAPWpm",
+    "G-HHRNPNB257",
+    "67836havfcB",
+    "21svydTQ",
+    "ripplid-ns.firebaseapp.com",
+    "1:346793201847:web:46dcd3b3311da7df2b3d32",
+    "ripplid-ns.appspot.com",
+    "ripplid-ns",
+    "4229757WpoWpP",
+    "346793201847",
+    "273372moCOPe",
+    "18715224mDyLYE",
+    "initializeApp",
+    "624XglITl",
+  ];
+  _0xbbe3 = function () {
+    return _0x33a7fe;
+  };
+  return _0xbbe3();
+}
+(function (_0x3f2737, _0x1823c9) {
+  const _0xc385f3 = _0x2a8c,
+    _0x5b13f6 = _0x3f2737();
+  while (!![]) {
+    try {
+      const _0x2d336c =
+        (-parseInt(_0xc385f3(0x11d)) / 0x1) *
+          (-parseInt(_0xc385f3(0x11e)) / 0x2) +
+        parseInt(_0xc385f3(0x128)) / 0x3 +
+        (-parseInt(_0xc385f3(0x122)) / 0x4) *
+          (-parseInt(_0xc385f3(0x130)) / 0x5) +
+        (parseInt(_0xc385f3(0x12f)) / 0x6) *
+          (-parseInt(_0xc385f3(0x123)) / 0x7) +
+        (-parseInt(_0xc385f3(0x12d)) / 0x8) *
+          (-parseInt(_0xc385f3(0x120)) / 0x9) +
+        (parseInt(_0xc385f3(0x11f)) / 0xa) *
+          (parseInt(_0xc385f3(0x12a)) / 0xb) +
+        -parseInt(_0xc385f3(0x12b)) / 0xc;
+      if (_0x2d336c === _0x1823c9) break;
+      else _0x5b13f6["push"](_0x5b13f6["shift"]());
+    } catch (_0x2995a7) {
+      _0x5b13f6["push"](_0x5b13f6["shift"]());
+    }
+  }
+})(_0xbbe3, 0xc29e3);
+function _0x2a8c(_0x32bd13, _0x2ae2d2) {
+  const _0xbbe330 = _0xbbe3();
+  return (
+    (_0x2a8c = function (_0x2a8c42, _0x9307e5) {
+      _0x2a8c42 = _0x2a8c42 - 0x11d;
+      let _0x5a9c31 = _0xbbe330[_0x2a8c42];
+      return _0x5a9c31;
+    }),
+    _0x2a8c(_0x32bd13, _0x2ae2d2)
+  );
+}
 const firebaseConfig = {
-  apiKey: "AIzaSyAg_duJHI5WZxInEmIfr3DqKsTTw5eJYvk",
-  authDomain: "ripplid-ns.firebaseapp.com",
-  projectId: "ripplid-ns",
-  storageBucket: "ripplid-ns.appspot.com",
-  messagingSenderId: "346793201847",
-  appId: "1:346793201847:web:46dcd3b3311da7df2b3d32",
-  measurementId: "G-HHRNPNB257"
+  apiKey: _0x5e7369(0x12e),
+  authDomain: _0x5e7369(0x124),
+  projectId: _0x5e7369(0x127),
+  storageBucket: _0x5e7369(0x126),
+  messagingSenderId: _0x5e7369(0x129),
+  appId: _0x5e7369(0x125),
+  measurementId: _0x5e7369(0x121),
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase[_0x5e7369(0x12c)](firebaseConfig);
